219ed6da8e70ccf875fe70a2a88e17087a03dd8f
ShrouqElsayed123 <shrouqwork123@gmail.com>
2025-05-07 15:57:37 +0300

    start in crypto component

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -1,8 +1,20 @@
+import { useGetCryptosQuery } from "../services/cryptoApi"
 
 export default function CryptoCurrencies() {
+
+  const {
+    data: cryptosList,
+    isFetching
+
+
+  } = useGetCryptosQuery()
+
+
+
+
   return (
-   <>
-   CryptoCurrencies
-   </>
+    <>
+      CryptoCurrencies
+    </>
   )
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,35 +1,45 @@
 import { Col, Row, Statistic, Typography } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import { NavLink } from 'react-router-dom';
+import CryptoCurrencies from "./CryptoCurrencies";
+import News from "./News";
+const { Title } = Typography;
+
 export default function HomePage() {
   const {
     data,
-    error,
-    isLoading,
-    isFetching,
-    isSuccess,
-    isError,
+
 
   } = useGetCryptosQuery()
-  if (isSuccess) {
-    console.log(data?.data?.coins);
-  }
 
 
 
 
+  const globalStats = data?.data?.stats
 
   return (
     <>
-      <Typography.Title level={2} >Global Crypto Stats </Typography.Title>
+      <Title level={2} >Global Crypto Stats </Title>
       <Row>
-        <Col span={12}><Statistic title="Unmerged" value={93} suffix="/ 100" /></Col>
-        <Col span={12}><Statistic title="Unmerged" value={93} suffix="/ 100" /></Col>
-        <Col span={12}><Statistic title="Unmerged" value={93} suffix="/ 100" /></Col>
-        <Col span={12}><Statistic title="Unmerged" value={93} suffix="/ 100" /></Col>
-        <Col span={12}><Statistic title="Unmerged" value={93} suffix="/ 100" /></Col>
-        <Col span={12}><Statistic title="Unmerged" value={93} suffix="/ 100" /></Col>
+        <Col span={12}><Statistic title="total" value={globalStats.total} suffix="/ 100" /></Col>
+        <Col span={12}><Statistic title="total24hVolume" value={globalStats.total24hVolume} suffix="/ 100" /></Col>
+        <Col span={12}><Statistic title="totalCoins" value={globalStats.totalCoins} suffix="/ 100" /></Col>
+        <Col span={12}><Statistic title="totalExchanges" value={globalStats.totalExchanges} suffix="/ 100" /></Col>
+        <Col span={12}><Statistic title="totalMarketCap" value={globalStats.totalMarketCap} suffix="/ 100" /></Col>
+        <Col span={12}><Statistic title="totalMarkets" value={globalStats.totalMarkets} suffix="/ 100" /></Col>
 
       </Row>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Top 10 cryptocurrencies In The World</Title>
+        <Title level={2} className="show-more"><NavLink to="/cryptocurrencies">Show more</NavLink></Title>
+      </div>
+      <CryptoCurrencies />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">Latest cryptocurrencies news</Title>
+        <Title level={2} className="show-more"><NavLink to="/news">Show more</NavLink></Title>
+      </div>
+      <News />
 
 
     </>
@@ -37,3 +47,6 @@ export default function HomePage() {
 
   )
 }
+
+
+
